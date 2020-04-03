46b0370c2059c583fc9ee0dc3dd4f6df6e92a95c
Karlo Smid <karlo.smid@ericsson.com>
2020-04-03 09:33:34 +0200

    add valid status list enum and enums module. We need that to report test case status.

---
+++ lib/test_link/enums.rb
@@ -0,0 +1,21 @@
+#    This file is part of testlink-api-client.
+#
+#    testlink-api-client is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    testlink-api-client is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with testlink-api-client.  If not, see <http://www.gnu.org/licenses/>.
+
+require 'test_link/enums/valid_status_list'
+
+module TestLink
+  module Enums
+  end
+end
